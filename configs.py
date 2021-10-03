a69af4d6405093828707171f44545dae5dc0832b
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-03 23:42:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,23 @@
 # (c) @Owdver
 
 import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            "gofilesbot.txt",
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 class Config(object):
