d8c059aa93c756ea2be61736bedbe517bf6691c5
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-07 12:52:17 +0530

    Update pm_query.py

--- plugins/pm_query.py
+++ plugins/pm_query.py
@@ -26,7 +26,7 @@ else:
 @Client.on_message(filters.private & filters.command("start"))
 async def start_handler(client: Bot, message: Message):
     await event.reply_text(
-        text=f"Hi, {event.from_user.mention}\n{Config.START_TEXT}",
+        text=f"Hi, {message.from_user.mention}\n{Config.START_TEXT}",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
@@ -42,46 +42,28 @@ async def start_handler(client: Bot, message: Message):
         )
     )
     try:
-        query_message = event.text.split(" ")[-1]
-        query_bytes = query_message.encode("ascii")
-        base64_bytes = b64decode(query_bytes)
-        secret_query = base64_bytes.decode("ascii")
-    except Exception:
-        msg = await bot.send_message(
-            chat_id=event.chat.id,
-            text=Config.HELP_TEXT,
-            reply_to_message_id=event.message_id
-        )
-        time.sleep(6)
-        try:
-            await msg.delete()
-            await event.delete()
-        except Exception:
-            pass
-        return
-    try:
-        await bot.send_message(
-            chat_id=event.chat.id,
-            text=Config.START_TEXT.format(event.from_user.first_name),
+        await client.send_message(
+            chat_id=message.chat.id,
+            text=Config.START_TEXT.format(message.from_user.mention),
             parse_mode='html',
             disable_web_page_preview=True
         )
         if secret_query:
             for channel in Config.CHANNELS:
                 # Looking for Document type in messages
-                async for messages in bot.USER.search_messages(channel, secret_query, filter="document", limit=50):
+                async for messages in client.USER.search_messages(channel, secret_query, filter="document", limit=50):
                     doc_file_names = messages.document.file_name
                     file_size = get_size(messages.document.file_size)
                     if re.compile(rf'{doc_file_names}', re.IGNORECASE):
                         media_name = messages.document.file_name.rsplit('.', 1)[0]
                         media_format = messages.document.file_name.split('.')[-1]
-                        await bot.send_chat_action(
+                        await client.send_chat_action(
                             chat_id=message.from_user.id,
                             action="upload_document"
                         )
                         try:
-                            await bot.copy_message(
-                                chat_id=event.chat.id,
+                            await client.copy_message(
+                                chat_id=message.chat.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
                                 caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_DOC.format(media_name,
@@ -90,18 +72,18 @@ async def start_handler(client: Bot, message: Message):
                         except FloodWait as e:
                             time.sleep(e.x)
                 # Looking for video type in messages
-                async for messages in bot.USER.search_messages(channel, secret_query, filter="video", limit=50):
+                async for messages in client.USER.search_messages(channel, secret_query, filter="video", limit=50):
                     vid_file_names = messages.caption
                     file_size = get_size(messages.video.file_size)
                     if re.compile(rf'{vid_file_names}', re.IGNORECASE):
                         media_name = secret_query.upper()
-                        await bot.send_chat_action(
-                            chat_id=event.from_user.id,
+                        await client.send_chat_action(
+                            chat_id=message.from_user.id,
                             action="upload_video"
                         )
                         try:
-                            await bot.copy_message(
-                                chat_id=event.chat.id,
+                            await client.copy_message(
+                                chat_id=message.chat.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
                                 caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
