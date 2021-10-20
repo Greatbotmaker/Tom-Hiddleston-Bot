e163ea04ff106e11ba15ecff260c525980d5c7a2
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-20 18:53:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -21,7 +21,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "@OB_LINKS")
