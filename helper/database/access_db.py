13a6377cf9eea31112a3a72a10f8cb187d2dbf94
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-20 21:34:51 +0530

    Update access_db.py

--- helper/database/access_db.py
+++ helper/database/access_db.py
@@ -1,6 +1,6 @@
 # (c) @AbirHasan2005
 
 from configs import Config
-from helpers.database.database import Database
+from helper.database.database import Database
 
 db = Database(Config.MONGODB_URI, Config.USER_SESSION)
