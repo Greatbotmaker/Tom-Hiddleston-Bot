bc9e20b9499fc6e3897f5ffa57ba1654599312d5
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-26 23:09:17 +0530

    Update pm_query.py

--- plugins/pm_query.py
+++ plugins/pm_query.py
@@ -10,6 +10,7 @@ from helper.file_size import get_size
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 if os.environ.get("ENV", False):
     from configs import Config
