3f71adf63c1d1d3fb54f1256947d590c5c4b2677
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-27 23:13:12 +0530

    Update user.py

--- user.py
+++ user.py
@@ -8,8 +8,8 @@ if os.environ.get("ENV", False):
     from configs import Config
     from configs import LOGGER
 else:
-    from configs import Config
-    from configs import LOGGER
+    from config import Config
+    from config import LOGGER
 
 class User(Client):
     def __init__(self):
@@ -17,7 +17,7 @@ class User(Client):
             Config.USER_SESSION,
             api_hash=Config.API_HASH,
             api_id=Config.APP_ID,
-            workers=40
+            workers=4
         )
         self.LOGGER = LOGGER
 
