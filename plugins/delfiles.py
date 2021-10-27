73e4c90f0f27e712d69e3e8848bd0629a33742c7
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-27 23:09:23 +0530

    Update delfiles.py

--- plugins/delfiles.py
+++ plugins/delfiles.py
@@ -13,7 +13,7 @@ from helper.custom_filter import allowed_chat_filter
 from pyrogram.errors import ChatAdminRequired
 
 if bool(os.environ.get("ENV", False)):
-    from sample_config import Config
+    from configs import Config
 else:
     from config import Config
 
