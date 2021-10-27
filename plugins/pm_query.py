182b7c6162abdf9762e5923e68654369b2c073d6
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-27 23:28:39 +0530

    Update pm_query.py

--- plugins/pm_query.py
+++ plugins/pm_query.py
@@ -1,4 +1,5 @@
-# -----------------------------------------------------------------|| https://github.com/owdver/DQ40 ||----------------------------------------------------------------- #
+# ---------------------------------- https://github.com/m4mallu/gofilesbot ------------------------------------------- #
+
 import re
 import os
 import time
@@ -10,10 +11,9 @@ from helper.file_size import get_size
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 if os.environ.get("ENV", False):
-    from configs import Config
+    from sample_config import Config
 else:
     from config import Config
 
@@ -23,22 +23,9 @@ async def bot_pm(client: Bot, message: Message):
     if message.text == "/start":
         await client.send_message(
             chat_id=message.chat.id,
-            text=f"Hi, {message.from_user.mention}\n{Config.START_TEXT}",
+            text=Presets.WELCOME_TEXT.format(message.from_user.first_name),
             parse_mode='html',
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton('➕ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕', url=f'http://t.me/OB_FILTEROBOT?startgroup=botstart')
-                    ],[
-                        InlineKeyboardButton('👨🏻‍💻 𝙲𝚁𝙴𝙰𝚃𝙾𝚁', url=f't.me/OWDVER_BOT'),
-                        InlineKeyboardButton('𝙲𝙷𝙰𝙽𝙽𝙴𝙻 📢', url=f't.me/OB_LINKS')
-                    ],[
-                        InlineKeyboardButton('🧑‍💻 DEV', url=f't.me/space4renjith'),
-                        InlineKeyboardButton('𝙷𝙴𝙻𝙿 ⚙️', callback_data="help")
-                    ]
-                ]
-            )
+            disable_web_page_preview=True
         )
         return
     try:
@@ -69,7 +56,7 @@ async def bot_pm(client: Bot, message: Message):
         if secret_query:
             for channel in Config.CHANNELS:
                 # Looking for Document type in messages
-                async for messages in client.USER.search_messages(channel, secret_query, filter="document", limit=10):
+                async for messages in client.USER.search_messages(channel, secret_query, filter="document", limit=50):
                     doc_file_names = messages.document.file_name
                     file_size = get_size(messages.document.file_size)
                     if re.compile(rf'{doc_file_names}', re.IGNORECASE):
@@ -84,13 +71,13 @@ async def bot_pm(client: Bot, message: Message):
                                 chat_id=message.chat.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=Config.BOTTOM_CAPTION+Presets.CAPTION_TEXT_DOC.format(media_name,
+                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_DOC.format(media_name,
                                                                                             media_format, file_size)
                             )
                         except FloodWait as e:
                             time.sleep(e.x)
                 # Looking for video type in messages
-                async for messages in client.USER.search_messages(channel, secret_query, filter="video", limit=10):
+                async for messages in client.USER.search_messages(channel, secret_query, filter="video", limit=50):
                     vid_file_names = messages.caption
                     file_size = get_size(messages.video.file_size)
                     if re.compile(rf'{vid_file_names}', re.IGNORECASE):
@@ -104,7 +91,7 @@ async def bot_pm(client: Bot, message: Message):
                                 chat_id=message.chat.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=Config.BOTTOM_CAPTION+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
+                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
                             )
                         except FloodWait as e:
                             time.sleep(e.x)
