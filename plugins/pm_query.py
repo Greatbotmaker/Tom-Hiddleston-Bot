0c03030e49f3fec085090cecdab52a1c3a801a88
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-27 22:20:06 +0530

    Update pm_query.py

--- plugins/pm_query.py
+++ plugins/pm_query.py
@@ -10,10 +10,10 @@ from helper.file_size import get_size
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 if os.environ.get("ENV", False):
-    from configs import Config
+    from sample_config import Config
 else:
     from config import Config
 
@@ -21,86 +21,92 @@ else:
 @Client.on_message(filters.private & filters.text)
 async def bot_pm(client: Bot, message: Message):
     if message.text == "/start":
-    await client.reply_text(
-        chat_id=message.chat.id,
-        text=f"Hi, {message.from_user.mention}\n{Config.START_TEXT}",
-        parse_mode='html',
-        quote=True,
-        disable_web_page_preview=True
-        reply_markup=InlineKeyboardMarkup(
-            [
+        await client.reply_text(
+            chat_id=message.chat.id,
+            text=f"Hi, {message.from_user.mention}\n{Config.START_TEXT}",
+            parse_mode='html',
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
-        InlineKeyboardButton('➕ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕', url=f'http://t.me/OB_FILTEROBOT?startgroup=botstart')
-        ],[
-        InlineKeyboardButton('👨🏻‍💻 𝙲𝚁𝙴𝙰𝚃𝙾𝚁', url=f't.me/OWDVER_BOT'),
-        InlineKeyboardButton('𝙲𝙷𝙰𝙽𝙽𝙴𝙻 📢', url=f't.me/OB_LINKS')
-    ],[
-        InlineKeyboardButton('🧑‍💻 DEV', url=f't.me/space4renjith'),
-        InlineKeyboardButton('𝙷𝙴𝙻𝙿 ⚙️', callback_data="help")]
-            ]
+                    [
+                        InlineKeyboardButton('➕ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕', url=f'http://t.me/OB_FILTEROBOT?startgroup=botstart')
+                    ],[
+                        InlineKeyboardButton('👨🏻‍💻 𝙲𝚁𝙴𝙰𝚃𝙾𝚁', url=f't.me/OWDVER_BOT'),
+                        InlineKeyboardButton('𝙲𝙷𝙰𝙽𝙽𝙴𝙻 📢', url=f't.me/OB_LINKS')
+                    ],[
+                        InlineKeyboardButton('🧑‍💻 DEV', url=f't.me/space4renjith'),
+                        InlineKeyboardButton('𝙷𝙴𝙻𝙿 ⚙️', callback_data="help")
+                    ]
+                ]
+            )
         )
-    )
-    return
-try:
-    if secret_query:
-        for channel in Config.CHANNELS:
-            # Looking for Document type in messages
-            async for messages in client.USER.search_messages(channel, secret_query, filter="document", limit=10):
-                doc_file_names = messages.document.file_name
-                file_size = get_size(messages.document.file_size)
-                if re.compile(rf'{doc_file_names}', re.IGNORECASE):
-                    media_name = messages.document.file_name.rsplit('.', 1)[0]
-                    media_format = messages.document.file_name.split('.')[-1]
-                    await client.send_chat_action(
-                        chat_id=message.from_user.id,
-                        action="upload_document"
-                    )
-                    try:
-                        await client.copy_message(
-                            chat_id=message.chat.id,
-                            from_chat_id=messages.chat.id,
-                            message_id=messages.message_id,
-                            caption=Config.BOTTOM_CAPTION+Presets.CAPTION_TEXT_DOC.format(media_name,
+        return
+    try:
+        query_message = message.text.split(" ")[-1]
+        query_bytes = query_message.encode("ascii")
+        base64_bytes = b64decode(query_bytes)
+        secret_query = base64_bytes.decode("ascii")
+    except Exception:
+        msg = await client.send_message(
+            chat_id=message.chat.id,
+            text=Presets.BOT_PM_TEXT,
+            reply_to_message_id=message.message_id
+        )
+        time.sleep(6)
+        try:
+            await msg.delete()
+            await message.delete()
+        except Exception:
+            pass
+        return
+    try:
+        await client.send_message(
+            chat_id=message.chat.id,
+            text=Presets.WELCOME_TEXT.format(message.from_user.first_name),
+            parse_mode='html',
+            disable_web_page_preview=True
+        )
+        if secret_query:
+            for channel in Config.CHANNELS:
+                # Looking for Document type in messages
+                async for messages in client.USER.search_messages(channel, secret_query, filter="document", limit=50):
+                    doc_file_names = messages.document.file_name
+                    file_size = get_size(messages.document.file_size)
+                    if re.compile(rf'{doc_file_names}', re.IGNORECASE):
+                        media_name = messages.document.file_name.rsplit('.', 1)[0]
+                        media_format = messages.document.file_name.split('.')[-1]
+                        await client.send_chat_action(
+                            chat_id=message.from_user.id,
+                            action="upload_document"
+                        )
+                        try:
+                            await client.copy_message(
+                                chat_id=message.chat.id,
+                                from_chat_id=messages.chat.id,
+                                message_id=messages.message_id,
+                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_DOC.format(media_name,
                                                                                             media_format, file_size)
+                            )
+                        except FloodWait as e:
+                            time.sleep(e.x)
+                # Looking for video type in messages
+                async for messages in client.USER.search_messages(channel, secret_query, filter="video", limit=50):
+                    vid_file_names = messages.caption
+                    file_size = get_size(messages.video.file_size)
+                    if re.compile(rf'{vid_file_names}', re.IGNORECASE):
+                        media_name = secret_query.upper()
+                        await client.send_chat_action(
+                            chat_id=message.from_user.id,
+                            action="upload_video"
                         )
-                    except FloodWait as e:
-                        time.sleep(e.x)
-            # Looking for photo type in messages
-            async for messages in client.USER.search_messages(channel, secret_query, filter="photo", limit=10):
-                pic_file_names = messages.caption
-                file_size = get_size(messages.photo.file_size)
-                if re.compile(rf'{pic_file_names}', re.IGNORECASE):
-                    await client.send_chat_action(
-                        chat_id=message.from_user.id,
-                        action="upload_photo"
-                    )
-                    try:
-                        await client.copy_message(
-                            chat_id=message.chat.id,
-                            from_chat_id=messages.chat.id,
-                            message_id=messages.message_id,
-                            caption=Config.BOTTOM_CAPTION+Presets.CAPTION_TEXT_PIC.format(media_name, file_size)
-                        )
-                    except FloodWait as e:
-                        time.sleep(e.x)
-            # Looking for video type in messages
-            async for messages in client.USER.search_messages(channel, secret_query, filter="video", limit=10):
-                vid_file_names = messages.caption
-                file_size = get_size(messages.video.file_size)
-                if re.compile(rf'{vid_file_names}', re.IGNORECASE):
-                    media_name = secret_query.upper()
-                    await client.send_chat_action(
-                        chat_id=message.from_user.id,
-                        action="upload_video"
-                    )
-                    try:
-                        await client.copy_message(
-                            chat_id=message.chat.id,
-                            from_chat_id=messages.chat.id,
-                            message_id=messages.message_id,
-                            caption=Config.BOTTOM_CAPTION+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
-                        )
-                    except FloodWait as e:
-                        time.sleep(e.x)
+                        try:
+                            await client.copy_message(
+                                chat_id=message.chat.id,
+                                from_chat_id=messages.chat.id,
+                                message_id=messages.message_id,
+                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
+                            )
+                        except FloodWait as e:
+                            time.sleep(e.x)
     except Exception:
         return
